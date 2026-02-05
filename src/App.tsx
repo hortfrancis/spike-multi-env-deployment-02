761b0fc42c42319dc417d9e576de589d76c864ab
Alex Hort-Francis <ajhf@hotmail.co.uk>
2026-02-05 03:15:23 +0000

    feat: Add additional text per dev & uat envs

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,16 @@ function App() {
     <div className='App'>
       <h1>Spike Multi-Env Deployment 01</h1>
       <p><em>Spike project to explore and describe how to handle automated deployment to multiple environments, using Git & GitHub.</em></p>
-      <p><strong>Current environment: <code>{ENVIRONMENT}</code></strong></p>
+      <p><strong>Current environment: <code className='highlight-environment'>{ENVIRONMENT}</code></strong></p>
+      {ENVIRONMENT === 'dev' && (
+        <p>This <code>dev</code> deployment will deploy on <strong>any push</strong> to branch <code>main</code>.</p>
+      )}
+      {ENVIRONMENT === 'uat' && (
+        <>
+          <p>This <code>uat</code> deployment will deploy when a <strong>tag with the pattern</strong> <code>v*-uat</code> is pushed.</p>
+          <p>For example, <code>v1.0.0-uat</code>.</p>
+        </>
+      )}
     </div>
   );
 };
